89d65307ad006abb1b570fc2a700067a2091a09b
Chris Edsall <chris.edsall@bristol.ac.uk>
2019-10-09 14:04:53 +0100

    Format with black

--- generate-calendar.py
+++ generate-calendar.py
@@ -1,15 +1,17 @@
 from datetime import date, timedelta
 
-sites = ['Bath',
-         'Bristol',
-         'Cardiff',
-         'Exeter']
-offset = 1 # start with Bristol
-start_date = date(2019,10,7)
+sites = ["Bath", "Bristol", "Cardiff", "Exeter"]
+offset = 1  # start with Bristol
+start_date = date(2019, 10, 7)
 weeks_to_schedule = 12
-format = '%a %d %b'
+format = "%a %d %b"
 for week in range(weeks_to_schedule):
-  monday = start_date + timedelta(days=(week * 7))
-  friday = monday + timedelta(days=4)
-  print(monday.strftime(format), "--", friday.strftime(format), "  ",
-        sites[(week + offset ) % 4])
+    monday = start_date + timedelta(days=(week * 7))
+    friday = monday + timedelta(days=4)
+    print(
+        monday.strftime(format),
+        "--",
+        friday.strftime(format),
+        "  ",
+        sites[(week + offset) % 4],
+    )
